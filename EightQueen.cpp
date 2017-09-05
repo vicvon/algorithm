c3a624b48a8b6d91bcd438f64290af9f6d5746af
vicktcfj <315516440@qq.com>
2017-09-05 09:42:56 +0800

    format

--- EightQueen.cpp
+++ EightQueen.cpp
@@ -14,56 +14,56 @@ vector<vector<int>> result;
 
 void searchQueen(int ithQueen)
 {
-	if (ithQueen == 8)
-	{
-		result.push_back(curResult);
-		return;
-	}
+    if (ithQueen == 8)
+    {
+        result.push_back(curResult);
+        return;
+    }
 
-	for (int i = 0; i < 8; ++i)
-	{
-		bool bPut = true;
-		if (pos[i] != 1)
-		{
-			curResult[ithQueen] = i + 1;
-		}
-		else
-		{
-			continue;
-		}
+    for (int i = 0; i < 8; ++i)
+    {
+        bool bPut = true;
+        if (pos[i] != 1)
+        {
+            curResult[ithQueen] = i + 1;
+        }
+        else
+        {
+            continue;
+        }
 
-		for (int j = 0; j < ithQueen; ++j)
-		{
-			if (curResult[j] == (i + 1) || abs((i + 1) - curResult[j]) == abs(ithQueen - j))
-			{
-				/* 可以被其他皇后吃掉 */
-				bPut = false;
-				break;
-			}
-		}
+        for (int j = 0; j < ithQueen; ++j)
+        {
+            if (curResult[j] == (i + 1) || abs((i + 1) - curResult[j]) == abs(ithQueen - j))
+            {
+                /* 可以被其他皇后吃掉 */
+                bPut = false;
+                break;
+            }
+        }
 
-		if (bPut)
-		{
-			pos[i] = 1;
-			searchQueen(ithQueen + 1);
-			pos[i] = 0;
-		}
-		curResult[ithQueen] = -1;
-	}
+        if (bPut)
+        {
+            pos[i] = 1;
+            searchQueen(ithQueen + 1);
+            pos[i] = 0;
+        }
+        curResult[ithQueen] = -1;
+    }
 }
 
 int main()
 {
-	curResult.assign(8, -1);
-	searchQueen(0);
+    curResult.assign(8, -1);
+    searchQueen(0);
 
-	for (int i = 1; i <= result.size(); ++i)
-	{
-		cout << i << ":";
-		for (int j = 0; j < result[i - 1].size(); ++j)
-			cout << result[i - 1][j] << ",";
-		cout << endl;
-	}
+    for (int i = 1; i <= result.size(); ++i)
+    {
+        cout << i << ":";
+        for (int j = 0; j < result[i - 1].size(); ++j)
+            cout << result[i - 1][j] << ",";
+        cout << endl;
+    }
 
-	cout << "八皇后最后共有" << result.size() << "种解法" << endl;
+    cout << "八皇后最后共有" << result.size() << "种解法" << endl;
 }
